e541f43ae1cb36bc1f8f429c0ab27528c6ec48e9
Baheya <khalifa.baheya@gmail.com>
2020-11-14 18:22:34 +0400

    added colors util

---
+++ utils/colors.js
@@ -0,0 +1,14 @@
+export const peach = {
+  primary: '#E7816B',
+  secondary: '#FFAD9B',
+};
+
+export const black = {
+  primary: '#1D1C1E',
+  secondary: '#333136',
+};
+
+export const white = {
+  primary: '#FFFFFF',
+  secondary: '#F1F3F5',
+};
