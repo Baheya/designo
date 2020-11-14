3c83d6f68b408b70272d3fc89341839d237649a5
Baheya <khalifa.baheya@gmail.com>
2020-11-14 18:23:17 +0400

    added typography util

---
+++ utils/typography.js
@@ -0,0 +1,8 @@
+export const primaryFont = '"Jost", sans-serif';
+
+export const typography = {
+  h1: { size: '48px', lineHeight: '48px', weight: '500' },
+  h2: { size: '40px', lineHeight: '48px', spacing: '2px', weight: '500' },
+  h3: { size: '20px', lineHeight: '26px', spacing: '5px', weight: '500' },
+  paragraph: { size: '16px', lineHeight: '26px', weight: '400' },
+};
