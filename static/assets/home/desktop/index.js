61d5904518bf34cfa9bc4800f80838cccb7bb855
Baheya <khalifa.baheya@gmail.com>
2020-11-22 18:05:05 +0400

    exported svg to use in index page

---
+++ static/assets/home/desktop/index.js
@@ -0,0 +1,9 @@
+import ResourcefulIllustration from './illustration-resourceful.svg';
+import PassionateIllustration from './illustration-passionate.svg';
+import FriendlyIllustration from './illustration-friendly.svg';
+
+export {
+  ResourcefulIllustration,
+  PassionateIllustration,
+  FriendlyIllustration,
+};
---
+++ static/assets/shared/desktop/index.js
@@ -0,0 +1,4 @@
+import ThreeCirclesBg from './bg-pattern-three-circles.svg';
+import CircleBg from './bg-pattern-small-circle.svg';
+
+export { ThreeCirclesBg, CircleBg };
