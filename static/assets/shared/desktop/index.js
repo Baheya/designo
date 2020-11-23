2ebe0e41f4cafdf7fff427e55c56b5bf34a3c146
Baheya <khalifa.baheya@gmail.com>
2020-11-23 16:09:37 +0400

    exporting social media icons

--- static/assets/shared/desktop/index.js
+++ static/assets/shared/desktop/index.js
@@ -1,4 +1,19 @@
 import ThreeCirclesBg from './bg-pattern-three-circles.svg';
 import CircleBg from './bg-pattern-small-circle.svg';
+// social media icons
 
-export { ThreeCirclesBg, CircleBg };
+import FacebookIcon from './icon-facebook.svg';
+import InstagramIcon from './icon-instagram.svg';
+import PinterestIcon from './icon-pinterest.svg';
+import TwitterIcon from './icon-twitter.svg';
+import YoutubeIcon from './icon-youtube.svg';
+
+export {
+  ThreeCirclesBg,
+  CircleBg,
+  FacebookIcon,
+  InstagramIcon,
+  PinterestIcon,
+  TwitterIcon,
+  YoutubeIcon,
+};
