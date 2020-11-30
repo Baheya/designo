e7feb060ce2a54c63395992a22190862ccdf7f20
Baheya <khalifa.baheya@gmail.com>
2020-11-30 17:13:38 +0400

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -6,45 +6,44 @@ import {
   ContactCard,
   Footer,
   NavCard,
+  AboutCard,
 } from '../components';
 
 const AppDesign = () => {
   return (
     <>
       <Head>
-        <title>Graphic Design</title>
+        <title>About</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles['grid']}>
         <section>
-          <HeroCard
-            title="Graphic Design"
-            text="We deliver eye-catching branding materials that are tailored to meet your business objectives."
-          />
-        </section>
-        <section>
           <ul>
-            <ProductCard
-              title="Tim Brown"
-              text="A book cover designed for Tim Brown’s new release, ‘Change’"
-              image="/static/assets/graphic-design/desktop/image-change.jpg"
+            <AboutCard
+              title="About us"
+              text="Founded in 2010, we are a creative agency that produces lasting results for our clients. 
+                We’ve partnered with many startups, corporations, and nonprofits alike to craft designs 
+                that make real impact. We’re always looking forward to creating brands, products, and 
+                digital experiences that connect with our clients' audiences."
+              bg="hero"
+              bgTheme="primary"
             />
-            <ProductCard
-              title="Boxed water"
-              text="A simple packaging concept made for Boxed Water"
-              image="/static/assets/graphic-design/desktop/image-boxed-water.jpg"
+            <AboutCard
+              title="World-class talent"
+              text="We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully 
+            crafted from concept to launch, ensuring success in its given market. We are constantly updating 
+            our skills in a myriad of platforms."
+              bg="talent"
+              bgTheme="secondary"
             />
-            <ProductCard
-              title="Science!"
-              text="A poster made in collaboration with the Federal Art Project"
-              image="/static/assets/graphic-design/desktop/image-science.jpg"
+            <AboutCard
+              title="The real deal"
+              text="As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own.
+                Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every
+                opportunity. We make design and technology more accessible and give you tools to measure success."
+              bg="realDeal"
+              bgTheme="secondary"
             />
-          </ul>
-        </section>
-        <section>
-          <ul>
-            <NavCard title="App Design" bg="appDesign" />
-            <NavCard title="Web Design" bg="webDesign" />
           </ul>
         </section>
         <section>
