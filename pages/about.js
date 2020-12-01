d87f8261c03ecb9e894e0190e03053f7047d8fa4
Baheya <khalifa.baheya@gmail.com>
2020-12-01 17:28:29 +0400

    changed prop style names to reflect updated card style

--- pages/about.js
+++ pages/about.js
@@ -19,23 +19,23 @@ const AppDesign = () => {
       </Head>
       <div className={styles['grid']}>
         <section>
-          <ul>
+          <ul className={styles['nested__grid']}>
             <AboutCard
               title="About us"
               text="Founded in 2010, we are a creative agency that produces lasting results for our clients. 
                 We’ve partnered with many startups, corporations, and nonprofits alike to craft designs 
                 that make real impact. We’re always looking forward to creating brands, products, and 
                 digital experiences that connect with our clients' audiences."
-              bg="hero"
-              bgTheme="primary"
+              img="hero"
+              theme="primary"
             />
             <AboutCard
               title="World-class talent"
               text="We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully 
             crafted from concept to launch, ensuring success in its given market. We are constantly updating 
             our skills in a myriad of platforms."
-              bg="talent"
-              bgTheme="secondary"
+              img="talent"
+              theme="secondary"
             />
             <li>
               <ul className={styles['locations__wrapper']}>
@@ -49,8 +49,8 @@ const AppDesign = () => {
               text="As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own.
                 Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every
                 opportunity. We make design and technology more accessible and give you tools to measure success."
-              bg="realDeal"
-              bgTheme="secondary"
+              img="realDeal"
+              theme="secondary"
             />
           </ul>
         </section>
