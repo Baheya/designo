90aa8d9796bab22ca4c92283963ada69e20a48ee
Baheya <khalifa.baheya@gmail.com>
2020-12-03 13:32:35 +0400

    removed main wrapper component

--- components/Shared/MainWrapper.js
+++
@@ -1,7 +0,0 @@
-import styles from './MainWrapper.module.scss';
-
-const MainWrapper = (props) => (
-  <main className={styles['wrapper']}>{props.children}</main>
-);
-
-export { MainWrapper };
--- components/index.js
+++ components/index.js
@@ -1,6 +1,5 @@
 export * from './Button/Button';
 export * from './Card/Card';
-export * from './Shared/MainWrapper';
 export * from './Navigation/Navigation';
 export * from './Footer/Footer';
 export * from './Location/Location';
